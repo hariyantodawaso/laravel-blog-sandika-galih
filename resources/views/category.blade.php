b31f70c18342e2101d6af65ff3072a082509c7e3
hariyanto dawaso <hariyantodawasoti14@gmail.com>
2022-04-28 01:34:38 +0800

    Part 8: Post Category & Eloquent Relationship

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.main')
 
 @section('container')
+    <h1 class="mb-3">Post Category: {{ $category }}</h1>
+
     @foreach ($posts as $post)
-        <article class="mb-5">
+        <article class="mb-3">
             <h2>
                 <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
