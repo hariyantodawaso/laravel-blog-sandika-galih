8b7c56e4d9fb4ac06d440a55d62c6d7bb6d2ffd4
hariyanto dawaso <hariyantodawasoti14@gmail.com>
2022-04-28 02:25:23 +0800

    Part 9. Database Seeder

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,21 +1,13 @@
  @extends('layouts.main')
 
  @section('container')
-     <article>
-         <h2>{{ $post->title }}</h2>
+     <h2>{{ $post->title }}</h2>
 
-         <p>By. Hariyanto Dawaso in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-         </p>
+     <p>By. Hariyanto Dawaso in <a href="/categories/{{ $post->category->slug }}"
+             class="text-decoration-none">{{ $post->category->name }}</a>
+     </p>
 
-         {!! $post->body !!}
-     </article>
-     <a href="/posts">Back to Posts</a>
+     {!! $post->body !!}
+
+     <a href="/posts" class="d-block mt-3 text-decoration-none">Back to Posts</a>
  @endsection
-
-
- {{-- Post::create([
-     'title' => 'Judul Kedua',
-     'slug' => 'judul-kedua',
-     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nulla natus iusto, nesciunt similique dolore explicabo, fuga error illum facilis ab voluptas quam corporis sunt soluta! Itaque totam ipsa sunt iste rem molestiae aspernatur ex, enim eaque repellat placeat quo nobis qui vitae deleniti cumque quibusdam voluptates quod quas distinctio velit sit ab sint! Eaque voluptas ad incidunt praesentium delectus temporibus ullam enim ratione dolores numquam quod esse in officiis similique dolorem aperiam illo ducimus, nam necessitatibus! Nemo voluptates culpa neque ipsam magni inventore nesciunt aspernatur provident expedita. Ea eaque sunt doloremque minus autem perspiciatis, nulla asperiores in enim soluta?'
- ]) --}}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,27 +1,22 @@
 @extends('layouts.main')
 
 @section('container')
+    <h1 class="mb-5">Halaman Blog Posts</h1>
+
     @foreach ($posts as $post)
-        <article class="mb-5">
+        <article class="mb-5 border-bottom pb-3">
             <h2>
                 <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
+
+            <p>By. <a class="text-decoration-none" href="#">{{ $post->user->name }}</a> in <a
+                    href="/categories/{{ $post->category->slug }}"
+                    class="text-decoration-none">{{ $post->category->name }}</a>
+            </p>
+
             <p>{{ $post->excerpt }}</p>
+
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more.. </a>
         </article>
     @endforeach
 @endsection
-
-
-{{-- Category::create([
-    'name' => 'Personal',
-    'slug' => 'personal',
-])
-
-
-Post::create([
-    'title' => 'Judul Ketiga',
-    'category_id' => 3,
-    'slug' => 'judul-ketiga',
-    'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    'body' => 'Nostrum optio nisi, quasi perferendis id laborum fugiat neque tempora voluptate repellendus! Et vitae at laboriosam impedit doloremque animi reiciendis minima, rerum provident esse, suscipit, ratione quae quasi odit adipisci ad accusantium. Magnam harum assumenda dicta distinctio maiores doloribus sit mollitia ipsum? Harum quisquam deleniti sequi deserunt quam totam minima odio necessitatibus delectus nesciunt quod illo, mollitia impedit fugit alias asperiores sapiente! Quam laboriosam voluptatibus corrupti ex minus iste nobis vel incidunt?',
-]) --}}
